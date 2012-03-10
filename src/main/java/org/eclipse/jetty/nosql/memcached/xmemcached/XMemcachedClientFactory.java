9a0835b9cdfdd51600033d7f1e69d499a45f35e7
Yamashita Yuu <yamashita@geishatokyo.com>
2012-03-11 00:39:34 +0900

    added BinaryXMemcachedClient

--- src/main/java/org/eclipse/jetty/nosql/memcached/xmemcached/XMemcachedClientFactory.java
+++ src/main/java/org/eclipse/jetty/nosql/memcached/xmemcached/XMemcachedClientFactory.java
@@ -4,10 +4,8 @@ import org.eclipse.jetty.nosql.kvs.AbstractKeyValueStoreClient;
 import org.eclipse.jetty.nosql.memcached.AbstractMemcachedClientFactory;
 
 public class XMemcachedClientFactory extends AbstractMemcachedClientFactory {
-
 	@Override
 	public AbstractKeyValueStoreClient create(String serverString) {
 		return new XMemcachedClient(serverString);
 	}
-
 }
